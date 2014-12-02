e661056eeadd3bf7763d4f2248843629c2c77599
Adolfo Olivera <adolfo.olivera@oliverasistemas.com>
2014-12-02 15:44:30 +0000

    mas cambios

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+
+	"password" => "El password debe contener por lo menos, 6 letras",
+
+	"user" => "No existe ningun usuario con esa direccion de correo",
+
+	"token" => "El token (codigo) no es valido",
+
+	"sent" => "El vinculo para reestablecer el password ha sido enviado!",
+
+	"reset" => "El password ha sido reestablecido!",
+
+];
